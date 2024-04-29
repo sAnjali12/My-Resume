c7aaf549c3956d8ebdfa4b8ff1899ba021f9895f
sAnjali12 <anjalisen060@gmail.com>
2024-04-29 11:56:20 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4H4JRX3M.mjs";import"./chunk-VU4ZPVLJ.mjs";import"./chunk-QMDJUXVP.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-RGNWAOKM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
