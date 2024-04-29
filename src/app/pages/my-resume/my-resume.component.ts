39f8045d70e88ad9e43cf4ce5985bd5b64c3aedf
sAnjali12 <anjalisen060@gmail.com>
2024-04-29 12:19:07 +0530

    add skilss part

--- src/app/pages/my-resume/my-resume.component.ts
+++ src/app/pages/my-resume/my-resume.component.ts
@@ -3,11 +3,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ThemeService } from '../../theme.service';
-import jspdf from 'jspdf';
-import html2canvas from 'html2canvas';
 import { CommonModule } from '@angular/common';
-
-// import html2pdf from 'html2pdf.js';
 
 
 @Component({
@@ -34,11 +30,9 @@ export class MyResumeComponent {
   @HostBinding('class') get themeMode() {
     return this.darkMode ? 'dark-theme' : '';
   }
-  @ViewChild('pdfTable') pdfTable: ElementRef;
   constructor(private themeService: ThemeService,
     private renderer: Renderer2) {
     this.isDarkMode = this.themeService.isDarkMode() 
-    this.pdfTable = {} as ElementRef;
 
   }
 
@@ -55,36 +49,36 @@ export class MyResumeComponent {
     }
   }
 
-  downloadAsPDF(){
-    let data = document.getElementById('contentToConvert');
-    let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-    const source = document.getElementById('contentToConvert');
-    if (source) {
-      html2canvas(source as HTMLCanvasElement, {
-        ignoreElements: (element) => {
-          return element.id === 'theme-btn' || element.id === 'pdf-button';
-        }
-      })
-        .then((canvas) => {
-          const imgData = canvas.toDataURL('image/jpeg', 1.0);
-          const pageWidth = pdf.internal.pageSize.getWidth();
-          const pageHeight = pdf.internal.pageSize.getHeight();
-          const imgWidth = canvas.width;
-          const imgHeight = canvas.height;
-          const ratio = Math.min(pageWidth / imgWidth, pageHeight / imgHeight);
+  // downloadAsPDF(){
+  //   let data = document.getElementById('contentToConvert');
+  //   let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+  //   const source = document.getElementById('contentToConvert');
+  //   if (source) {
+  //     html2canvas(source as HTMLCanvasElement, {
+  //       ignoreElements: (element) => {
+  //         return element.id === 'theme-btn' || element.id === 'pdf-button';
+  //       }
+  //     })
+  //       .then((canvas) => {
+  //         const imgData = canvas.toDataURL('image/jpeg', 1.0);
+  //         const pageWidth = pdf.internal.pageSize.getWidth();
+  //         const pageHeight = pdf.internal.pageSize.getHeight();
+  //         const imgWidth = canvas.width;
+  //         const imgHeight = canvas.height;
+  //         const ratio = Math.min(pageWidth / imgWidth, pageHeight / imgHeight);
     
-          const newWidth = imgWidth * ratio;
-          const newHeight = imgHeight * ratio;
+  //         const newWidth = imgWidth * ratio;
+  //         const newHeight = imgHeight * ratio;
     
-          pdf.addImage(imgData, 'JPEG', 0, 0, newWidth, newHeight);
-        const pdfWindow = window.open('', '_blank');
-        pdfWindow?.document.write('<html><body><embed src="' + pdf.output('datauristring') + '" type="application/pdf" width="100%" height="90%"></embed></body></html>');    
-        pdf.save('Anjali.pdf'); // Generated PDF
-        });
-    } else {
-      console.error("Element with ID 'contentToConvert' not found.");
-    }
-  }
+  //         pdf.addImage(imgData, 'JPEG', 0, 0, newWidth, newHeight);
+  //       const pdfWindow = window.open('', '_blank');
+  //       pdfWindow?.document.write('<html><body><embed src="' + pdf.output('datauristring') + '" type="application/pdf" width="100%" height="90%"></embed></body></html>');    
+  //       pdf.save('Anjali.pdf'); // Generated PDF
+  //       });
+  //   } else {
+  //     console.error("Element with ID 'contentToConvert' not found.");
+  //   }
+  // }
 
 
   socialLink(type: string){
